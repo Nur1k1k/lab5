92fbe6cc7f9d619d503c3bde568432e2dadaf553
Nurkozha <31116@iitu.edu.kz>
2024-03-21 22:00:48 +0500

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueAccent,
-        appBar: AppBar(
-          title: Text("My first App"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(value: 23),
-                Text(
-                  '23%',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                Text(
-                  'Press button to download',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              ],
+      home: FirstScreen(),
+    );
+  }
+}
+
+//First screen
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text("First Screen"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+              child: Text("Go to Second Screen"),
             ),
-          ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.cloud_download),
+      ),
+    );
+  }
+}
+
+//Second screen
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text("Second Screen"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Back to First Screen"),
+            ),
+          ],
         ),
       ),
     );
