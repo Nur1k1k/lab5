070311bc7913efd0fc9e8fcc0cea5dc4de87548b
Nurkozha <31116@iitu.edu.kz>
2024-03-31 21:20:43 +0500

    lab6

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirstScreen(),
+      home: MyTabbedApp(),
     );
   }
 }
 
-//First screen
+class MyTabbedApp extends StatefulWidget {
+  @override
+  _MyTabbedAppState createState() => _MyTabbedAppState();
+}
+
+class _MyTabbedAppState extends State<MyTabbedApp> {
+  int _currentIndex = 0;
+  final List<Widget> _tabs = [
+    FirstScreen(),
+    SecondScreen(),
+    // Добавьте другие экраны по желанию
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          // Добавьте другие вкладки по желанию
+        ],
+      ),
+    );
+  }
+}
+
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -26,13 +65,16 @@ class FirstScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
+              onPressed: () async {
+                final result = await Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SecondScreen()),
                 );
+                // При возвращении данных на первый экран
+                // можно их обработать здесь
+                print('Data received on first screen: $result');
               },
-              child: Text("Go to Second Screen"),
+              child: Text("Go to Second Screen and Return Data"),
             ),
           ],
         ),
@@ -41,7 +83,6 @@ class FirstScreen extends StatelessWidget {
   }
 }
 
-//Second screen
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -57,9 +98,9 @@ class SecondScreen extends StatelessWidget {
           children: <Widget>[
             ElevatedButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, 'Data from Second Screen');
               },
-              child: Text("Back to First Screen"),
+              child: Text("Back to First Screen with Data"),
             ),
           ],
         ),
